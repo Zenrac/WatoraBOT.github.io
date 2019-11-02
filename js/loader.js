c4be3e071f0cc085a8123c60f1f281b431badc76
Zenrac <zenrac@outlook.fr>
2019-11-02 17:04:10 +0100

    Force https

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,10 @@
-function loadScript (source) {
+function loadScript (source, ensure_http = true) {
    var scr = document.createElement("script");
+   if (ensure_http) {
+     if (!source.includes('https')) {
+       source = source.replace('http', 'https');
+     }
+   }
    scr.src = source;
    document.body.appendChild(scr);
 }
